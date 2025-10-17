5d39a76122b8eb58a492d16fa751a586392f508f
Quizzer <quizzer1pro@gmail.com>
2025-10-17 19:32:04 +0545

    Add files via upload

---
+++ day2variables&values.js
@@ -0,0 +1,15 @@
+var diNesh = "din";
+console.log(diNesh);
+
+
+var myAge = 22;
+console.log(myAge);
+
+var _myD = "dines"
+console.log(_myD)
+// var 123myAge = 22;
+// console.log(123myAge);     //it cannot support number in variables name start
+
+
+// var my@Email = "dinesh@hdkd";
+// console.log(my@Email);       //@ is special character so it doesnot works either you use underscore no letter $
